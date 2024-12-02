4b5dc06088c9d21251260c680a4108d506ddf6d9
Park won-gyu <snupark@snu.ac.kr>
2024-12-02 16:47:36 +0900

    povray init 05

--- ccelkit/cli.py
+++ ccelkit/cli.py
@@ -17,7 +17,7 @@ def main():
     parser_visual.add_argument("-o","--output_filepath",type=str,default=None,help="output image file path")
     parser_visual.add_argument("-r","--repeat_atom",type=int,nargs='+',default=[1,1,1],help="repeatation")
     parser_visual.add_argument("-ori","--orientation",type=float,nargs='+',default=[+0.955480,+0.294974,-0.006949,-0.042581,+0.161156,+0.986010,+0.291967,-0.941817,+0.166542],help="camera orientation")
-    parser_visual.add_argument("-c","--cell_on",action='store_true',help="visual cell")
+    parser_visual.add_argument("--cell_on",action='store_true',help="visual cell")
     parser_visual.add_argument("-t","--transmittances",type=float,nargs='+',default=None,help="atom transmittances")
     parser_visual.add_argument("-H","--heatmaps",type=float,nargs='+',default=None,help="atom heatmaps")
     parser_visual.add_argument("-w","--canvas_width",type=int,default=960,help="pixel of width")
